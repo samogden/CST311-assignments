30e0468a9f40d20538001f5f57d376a35fdf2b98
halHalberstadt <90930042+halHalberstadt@users.noreply.github.com>
2022-10-20 12:43:03 -0700

    Added required functionality
    - the server takes in a connection from both clients
    - the server then takes in input from both connections
    - the server then formats a return string in the order sent in
    - once both entries are in the server, it then sends both clients the resulting string

--- PA3/src/server.py
+++ PA3/src/server.py
@@ -1,15 +1,17 @@
 #!env python
 
 """Chat server for CST311 Programming Assignment 3"""
-__author__ = "[team name here]"
+__author__ = "[team 1]"
 __credits__ = [
-  "Your",
-  "Names",
-  "Here"
+  "Aiden Rougot",
+  "Nayan Gupta",
+  "Jonathan McCaslin", 
+  "Hal Halberstadt"  
 ]
 
 
 import socket as s
+import threading
 
 # Configure logging
 import logging
@@ -18,26 +20,20 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 server_port = 12000
+response = ""
+temp = 0
 
-def connection_handler(connection_socket, address):
+def connection_handler(connection_socket, address, query_decoded):
   # Read data from the new connectio socket
   #  Note: if no data has been sent this blocks until there is data
-  query = connection_socket.recv(1024)
-  
-  # Decode data from UTF-8 bytestream
-  query_decoded = query.decode()
-  
   # Log query information
+  globals()['temp'] += 1
   log.info("Recieved query test \"" + str(query_decoded) + "\"")
   
   # Perform some server operations on data to generate response
-  response = query_decoded.upper()
+  globals()['response'] += str(address) + ":" + query_decoded.upper() + " "
+    
   
-  # Sent response over the network, encoding to UTF-8
-  connection_socket.send(response.encode())
-  
-  # Close client socket
-  connection_socket.close()
   
 
 def main():
@@ -49,20 +45,34 @@ def main():
   server_socket.bind(('',server_port))
   
   # Configure how many requests can be queued on the server at once
-  server_socket.listen(1)
+  server_socket.listen(2)
   
   # Alert user we are now online
   log.info("The server is ready to receive on port " + str(server_port))
+  s_c = []
   
   # Surround with a try-finally to ensure we clean up the socket after we're done
   try:
     # Enter forever loop to listen for requests
-    while True:
-      # When a client connects, create a new socket and record their address
-      connection_socket, address = server_socket.accept()
-      log.info("Connected to client at " + str(address))
-      # Pass the new socket and address off to a connection handler function
-      connection_handler(connection_socket, address)
+    for _ in range(2):
+      while globals()['temp'] < 2:
+        # When a client connects, create a new socket and record their address
+        connection_socket, address = server_socket.accept()
+        s_c.append([connection_socket, address])
+        log.info("Connected to client at " + str(address))
+        # Pass the new socket and address off to a connection handler function
+        query = connection_socket.recv(1024)
+    
+        # Decode data from UTF-8 bytestream
+        query_decoded = query.decode()
+    
+        connection_handler(connection_socket, address, query_decoded)
+    for con in s_c:
+      # Sent response over the network, encoding to UTF-8
+      con[0].send(globals()['response'].encode())
+      
+      # Close client socket
+      con[0].close()
   finally:
     server_socket.close()
 
